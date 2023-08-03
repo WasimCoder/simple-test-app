1d040349939b11ca13ab8f6b46f1dc2b5c350fd4
Wasim <wasimakthar.work@gmail.com>
2023-08-03 15:18:02 +0530

    Third Commit - Data correctly fetched from Service

--- public-ip/src/app/components/middle/middle.component.ts
+++ public-ip/src/app/components/middle/middle.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit} from '@angular/core';
+import { Observable } from 'rxjs';
+import { IPInterface } from 'src/app/interfaces/interfaces';
 import { IpService } from 'src/app/services/ip.service';
 
 @Component({
@@ -8,12 +10,13 @@ import { IpService } from 'src/app/services/ip.service';
   styleUrls: ['./middle.component.scss']
 })
 export class MiddleComponent implements OnInit{
-constructor(private http: HttpClient, private ip: IpService){}
-data:any
+constructor(private http: HttpClient, private ip: IpService){ }
+  data!:any;
 ngOnInit() : void{
-  this.ip.getIpData('182.156.212.133').subscribe((response:any)=>{
+  this.ip.getIpData('182.156.212.133').subscribe((response:Object)=>{
       console.log(response)
-      response = this.data
+      this.data= response
+      console.log(this.data)
     })
 }
 }
